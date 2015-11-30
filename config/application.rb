0aebef189093a3935a3c1735692a714de8745a46
Henry Dotson <l3iodeez@gmail.com>
2015-11-30 15:45:44 -0500

    Fix paperclip/heroku issues.

--- config/application.rb
+++ config/application.rb
@@ -25,13 +25,5 @@ module Clevernote
     config.tinymce.install = :compile
     config.active_job.queue_adapter = :delayed_job
 
-    config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => ENV["s3_bucket"],
-        :access_key_id => ENV["s3_access_key_id"],
-        :secret_access_key => ENV["s3_secret_access_key"]
-      }
-    }
   end
 end
